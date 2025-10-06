d2a4adf751449f03669e45d225912f8b6388c4db
Mehrzad Naghiei <mehrzad.mahbod@gmail.com>
2025-10-06 16:15:26 +0330

    ng2ChartsAdded

--- src/app/+pages/admins/admin-dashboard/admin-dashboard.component.ts
+++ src/app/+pages/admins/admin-dashboard/admin-dashboard.component.ts
@@ -7,10 +7,11 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatDivider } from "@angular/material/divider";
+import { MatDivider } from '@angular/material/divider';
 import { MatRadioModule } from '@angular/material/radio';
 import { FormsModule } from '@angular/forms';
-
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -25,18 +26,57 @@ import { FormsModule } from '@angular/forms';
     MatCardModule,
     MatDivider,
     MatRadioModule,
-    FormsModule
-]
+    FormsModule,
+    BaseChartDirective,
+  ],
 })
 export class AdminDashboardComponent {
 
-  ChoosenEntity : string = 'users';
+count() {
+  this.barChartData.datasets[0].data[0] = 10 ;
+}
+  ChoosenEntity: string = 'Users';
   entities: string[] = ['Users', 'Students', 'Faculties'];
 
+  barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: true,
+    scales: {
+      x: {
+        title: {
+          display: false,
+          text: 'Day of the Week',
+        },
+      },
+      y: {
+        beginAtZero: false,
+        title: {
+          display: true,
+          text: 'Active ' + this.ChoosenEntity,
+        },
+      },
+    },
+  };
 
+  barChartLabels: string[] = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
 
-
-
+  barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: this.barChartLabels,
+    datasets: [
+      {
+        data: [12, 19, 7, 5, 14, 8, 10],
+        label: this.ChoosenEntity + ' Count',
+        backgroundColor: '#005cbb',
+      },
+    ],
+  };
 
   private breakpointObserver = inject(BreakpointObserver);
 
@@ -48,7 +88,7 @@ export class AdminDashboardComponent {
           { title: 'SmartCampus Statics', cols: 1, rows: 1 },
           { title: 'Card 2', cols: 1, rows: 1 },
           { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'Card 4', cols: 1, rows: 1 },
         ];
       }
 
@@ -56,7 +96,7 @@ export class AdminDashboardComponent {
         { title: 'SmartCampus Statics', cols: 2, rows: 1 },
         { title: 'Card 2', cols: 1, rows: 1 },
         { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: 'Card 4', cols: 1, rows: 1 },
       ];
     })
   );
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ErrorInterceptor } from './+interceptors/error-interceptor/error-interceptor';
 import { AuthInterceptor } from './+interceptors/auth-interceptor/auth-interceptor';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 
 export const appConfig: ApplicationConfig = {
@@ -12,7 +13,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
+    provideCharts(withDefaultRegisterables()),
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }, provideCharts(withDefaultRegisterables())
   ]
 };
